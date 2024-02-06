2b122051085a5637f182b845abfe6b94ed74630c
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2024-02-06 00:05:56 -0500

    Added third tab and cleaned up some old boilerplate CSS

--- AugmentedSpace/app/(tabs)/ar_view.tsx
+++ AugmentedSpace/app/(tabs)/ar_view.tsx
@@ -1,15 +1,14 @@
 import { Text, View } from "@/components/Themed";
 
-export default function Index() {
+export default function AR_View() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="font-bold text-xl">Index Page</Text>
+      <Text className="font-bold text-xl">This is the AR View Page</Text>
       <View
         className="my-8 h-1 w-4/5"
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <Text>Expected to see catelog/main page here.</Text>
     </View>
   );
 }
--- AugmentedSpace/app/(tabs)/cart.tsx
+++ AugmentedSpace/app/(tabs)/cart.tsx
@@ -1,16 +1,14 @@
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-export default function TabTwoScreen() {
+export default function Cart() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="font-bold text-xl">Tab Two</Text>
+      <Text className="font-bold text-xl">This Is Your Cart</Text>
       <View
         className="my-8 h-1 w-4/5"
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
