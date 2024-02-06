53bc687f85803b1a81aa7bf8ee85ffc3a3d158cb
Jessee Horton <61556428+Jessku@users.noreply.github.com>
2024-02-05 21:11:54 -0500

    Testing

---
+++ Unity-Project/AugmentedSpace/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Power isn't everything");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
