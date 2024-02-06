a0be29a4fd71131dc22a8c18fb24489997f58f40
rory lange <rorystrattonlange@gmail.com>
2024-02-06 15:14:39 -0500

    created development branch, changed some placeholder values on index.tsx

--- AugmentedSpace/app/(tabs)/index.tsx
+++ AugmentedSpace/app/(tabs)/index.tsx
@@ -3,13 +3,13 @@ import { Text, View } from "@/components/Themed";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="font-bold text-xl">Index Page</Text>
+      <Text className="font-bold text-xl">Augmented Space</Text>
       <View
         className="my-8 h-1 w-4/5"
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <Text>Expected to see catelog/main page here.</Text>
+      <Text>Add login page and buttons here</Text>
     </View>
   );
 }
