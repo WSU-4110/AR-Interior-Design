95b57a8f459eb375f855725c4e634c077a2ff526
rory lange <rorystrattonlange@gmail.com>
2024-03-13 16:27:01 -0400

    fixed card styles, changed styling to tailwind, added darkmode support for catalog page, adjusted styling and colors for darkmode on catalog page.

--- AugmentedSpace/components/ItemCard.tsx
+++ AugmentedSpace/components/ItemCard.tsx
@@ -1,4 +1,4 @@
-import { Image, Pressable, Text } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 import { useTheme } from "@react-navigation/native";
 
 type ItemCardProps = {
@@ -13,28 +13,31 @@ export default function ItemCard(props: ItemCardProps) {
   const { colors } = useTheme();
   return (
     <Pressable
-      className="w-1/2 grow mx-1 p-2"
+      className="w-1/2 h-auto flex-1 mx-1 p-2 rounded-2xl"
       onPress={props.onPress}
       style={{
         backgroundColor: colors.card,
         shadowOffset: { width: 2, height: 2 },
-        shadowColor: "#333",
+        shadowColor: colors.shadow,
         shadowOpacity: 1,
       }}
     >
       <Image
-        className="self-center"
+        className="self-center h-32 w-32 my-2"
         source={require("@/assets/images/favicon.png")}
       />
-      <Text className="text-xl" style={{ color: colors.text }}>
-        {props.itemName}
-      </Text>
-      <Text className="italic font-semibold" style={{ color: colors.text }}>
-        {props.brandName}
-      </Text>
-      <Text className="font-semibold" style={{ color: colors.text }}>
-        {props.itemCost}
-      </Text>
+      <View
+        className="m-1">
+        <Text className="text-xl" style={{ color: colors.text }}>
+          {props.itemName}
+        </Text>
+        <Text className="italic font-semibold" style={{ color: colors.text }}>
+          {props.brandName}
+        </Text>
+        <Text className="font-semibold" style={{ color: colors.text }}>
+          {props.itemCost}
+        </Text>
+      </View>
     </Pressable>
   );
 }
