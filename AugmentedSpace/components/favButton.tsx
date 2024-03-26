31ca79e3204b4e6e7c7d0b54dfbe2cd271e896f4
rory lange <rorystrattonlange@gmail.com>
2024-03-26 14:47:48 -0400

    added favorite button on the top of the itemCard, created component and variables for card, needs backend integration.

---
+++ AugmentedSpace/components/favButton.tsx
@@ -0,0 +1,43 @@
+import { useTheme } from "@react-navigation/native";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, View } from "react-native"
+import {MaterialCommunityIcons} from "@expo/vector-icons"
+
+type FavButtonProps = {
+    onPress?: () => void;
+    className?: string;
+}
+export default function FavButton(props: FavButtonProps) {
+    const { colors } = useTheme();
+    const [liked, setLiked] = useState(false);
+    
+
+    return (
+        <View
+            className=" flex-auto rounded-full items-center p-1 self-end"
+            style={{
+                backgroundColor: colors.card,
+                position: "absolute",
+                shadowOffset: { width:0, height:2 },
+                shadowColor: colors.shadow,
+                shadowOpacity: 1,
+
+            }}
+            >
+            <Pressable
+                onPress={ () => setLiked(!liked) }
+            >
+                <MaterialCommunityIcons
+                    name={liked ? "heart" : "heart-outline"}
+                    size={32}
+                    color={liked ? "red" : "gray"}
+                    />
+            </Pressable>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
