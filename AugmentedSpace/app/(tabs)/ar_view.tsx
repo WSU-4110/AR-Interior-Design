438db79e8bbffc87174a283506a1c8d950ee70ed
kasataya <kassemataya0@gmail.com>
2024-04-11 16:55:22 -0400

    Ke

--- AugmentedSpace/app/(tabs)/ar_view.tsx
+++ AugmentedSpace/app/(tabs)/ar_view.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { View } from "react-native";
-import { Camera } from "expo-camera";
 
 const UnityWithExpoCamera = () => {
   return (
     <View style={{ flex: 1 }}>
-      <Camera
-        style={{ flex: 1 }}
-        type={"back"} // Specify 'back' as a string here
-        autoFocus={Camera.Constants.AutoFocus.on}
-        flashMode={Camera.Constants.FlashMode.off}
-      />
+      
     </View>
   );
 };
@@ -18,5 +12,5 @@ const UnityWithExpoCamera = () => {
 export default UnityWithExpoCamera;
 
 
-export default UnityWithExpoCamera;
+
 
