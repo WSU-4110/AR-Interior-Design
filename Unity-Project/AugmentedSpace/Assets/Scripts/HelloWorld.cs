7dac2a749c149f4ed5d3bb8150832e5592910383
Jessee Horton <61556428+Jessku@users.noreply.github.com>
2024-04-12 00:12:37 -0400

    unittest

--- Unity-Project/AugmentedSpace/Assets/Scripts/HelloWorld.cs
+++ Unity-Project/AugmentedSpace/Assets/Scripts/HelloWorld.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public string start1 = "Power isn't everything";
+    public string start2 = "力が全てじゃない";
     // Start is called before the first frame update
     void Start()
     {
---
+++ Unity-Project/AugmentedSpace/Assets/Scripts/Tests/EditMode/HelloWorld-test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class HelloWorld-test
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void HelloWorld-testSimplePasses()
+    {
+      //21qwqq23333333333333333333333333333333333333333333333 Assert.AreEqual("Power isn't everything", )
+    }
+}
